99f3d9c56855dfcf402cde1fee109c6ba663157a
David <davidongaroh@gmail.com>
2022-08-17 14:45:37 +0300

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+    attr_accessor :name ,:breed
+    def initialize(name,breed="Mutt")
+        @name= name
+        @breed = breed 
+    end
+end
+
+kibe = Dog.new("Kibe", "Siberian Husk")
+puts kibe.breed
